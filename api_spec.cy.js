77dad5cdb1da08f41257f615899124e653098f79
Maishmike <mikemairush55@gmail.com>
2024-02-26 12:53:24 +0300

    API tests

---
+++ api_spec.cy.js
@@ -0,0 +1,84 @@
+describe('template spec', () => {
+	/*beforeEach(() => {
+		cy.visit('localhost:3000')
+	})
+	
+	it.only('passes', () => {
+		cy.getByDataTest('email-input').type('john@example.com')
+		cy.getByDataTest('submit-button').click()
+		cy.getByDataTest('server-error-message').should('contain', 'already exists')
+	})
+	
+	it('Makes a get request', () => {
+		cy.request('GET', 'https://bstackdemo.com/').then((response) => {
+			expect(response.headers.connection).to.eq('keep-alve')
+		})
+	})*/
+	
+	it('Sends a POST', () => {
+		cy.request({
+			method : 'POST',
+			url: 'https://reqres.in/api/users',
+			body:{
+				name : 'test1212',
+				job : 'boss',
+				id : '123456'
+			}
+		}).then((response) => {
+
+				expect(response.status).to.eq(201)
+				expect(response.body.name).to.eq('test1212')
+				cy.url().log()
+
+		})
+	})	
+	
+
+	
+	it('Gets a ZamuPay auth token', () => {
+		cy.request({
+			method : 'POST',
+			url: 'https://auth.zamupay.com/connect/token',
+			form : true,
+			body:{
+				grant_type: 'client_credentials',
+				client_id: 'Omega',
+				client_secret: 'Omega',
+				scope: 'PyPay_api'
+			}
+			
+		}).then((response) => {
+			cy.log(response.body.access_token)
+			cy.writeFile('cypress/fixtures/token.txt', response.body.access_token)
+		})
+	})
+	
+	it('Purchases airtime', ()=> {
+		cy.fixture('token.txt', {timeout : 20000}).then((token) => {
+			const originatorConversationId = Cypress._.random(0, 1e6);
+			cy.request({
+				method : 'POST',
+				url: 'https://sandboxapi.zamupay.com/v1/airtime-purchase',
+				headers: {
+					Authorization : `Bearer ${token}`
+				},
+				body: {
+					originatorConversationId: originatorConversationId,
+					callbackURL: "https://en1ezbmu2vsd.x.pipedream.net",
+					recipients:
+					[
+						{
+							currencyCode: "KES",
+							phoneNumber: "254700000000",
+							amount: 99,
+							name: "Jojo Siwa"
+						}
+					]
+				}
+			})
+		})
+	})
+	
+	
+  
+})
